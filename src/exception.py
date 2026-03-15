376db5cb0aa4029406760c54c144086cecd9e384
ashuwebdevloper <aboycalledashish@gmail.com>
2026-03-16 00:25:47 +0530

    5

--- src/exception.py
+++ src/exception.py
@@ -1,14 +1,22 @@
-import sys 
-##  provides various funstions to handle exception in a better way  
+import sys
 
-def error_message_detail(error,error_detail:sys):
-    _,_,error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="error occured in python script name [{0}] line number [{1}] error message [{2}]".format(error_detail[0].__name__,error_detail[2].tb_lineno,str(error))
-    file_name,exc_tb.tb_lineo,str(error)
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+
+    return error_message
 
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os 
+import sys    
+from datetime import datetime   
+
+from src.exception import CustomException
+
+LOG_FILE=f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE) 
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+## these allla re not as compussory these are the basic configuration for logging
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
